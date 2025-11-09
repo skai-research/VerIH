615c975bbc9c999011fe8469647564853e650f86
ZetaRing <windwing.me@gmail.com>
2025-11-08 21:31:47 -0500

    Unset ROCR_VISIBLE_DEVICES before training

--- RLVR/run.sh
+++ RLVR/run.sh
@@ -4,4 +4,5 @@ export TEMPLATED_INPUT=1
 export BASE_MODEL="Qwen/Qwen3-8B"
 export DATA_DIR="dataset/qwen3-verih-cot_syshint"
 export EXPERIMENT_NAME="Qwen3-8B-GRPO-01R-2048-verih"
+unset ROCR_VISIBLE_DEVICES
 bash ./scripts/train_grpo.sh
